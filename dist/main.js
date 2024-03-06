ce7d837d359129ec50a9ca6da5b9de5071a631c3
SeanPhilippi <sean.philippi@gmail.com>
2024-03-05 19:40:42 -0500

    added webpack.config.js and tailwind files and deps, seems to be working

--- dist/main.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";console.log(Math.random().toString(36).substring(7))})();
--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import generateCode from './generateCode';
+import './style.css';
+
 console.log(generateCode());
